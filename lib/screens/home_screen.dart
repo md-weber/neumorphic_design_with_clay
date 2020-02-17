efa949654b8f7c947375a6c13b8fe9507b48a6f5
myracledesign <weber.max.1988@live.de>
2020-02-17 08:05:12 +0100

    first neumorphic button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,32 @@
+import 'package:clay_containers/clay_containers.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Center(),
-    ));
+      backgroundColor: Colors.green,
+      body: Container(
+        child: Center(
+          child: ClayContainer(
+            height: 150,
+            width: 150,
+            surfaceColor: Colors.green,
+            parentColor: Colors.green,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClayContainer(
+                color: Colors.green,
+                borderRadius: 50,
+                child: Icon(
+                  Icons.pie_chart,
+                  size: 100,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
