63ba51713de6705d3122e5cbe609afa166a6d675
myracledesign <weber.max.1988@live.de>
2020-02-17 17:20:46 +0100

    created a new widget and put it on the bottom of the page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,28 +2,87 @@ import 'package:clay_containers/clay_containers.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  Color baseColor = Color(0xFF464646);
+  Color navColor = Color(0xFF2c2c2c);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
-      body: Container(
-        child: Center(
-          child: ClayContainer(
-            height: 150,
-            width: 150,
-            surfaceColor: Colors.green,
-            parentColor: Colors.green,
+      backgroundColor: baseColor,
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Column()),
+          Container(
+            color: navColor,
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ClayContainer(
-                color: Colors.green,
-                borderRadius: 50,
-                child: Icon(
-                  Icons.pie_chart,
-                  size: 100,
-                ),
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ClayButtonWidget(
+                    baseColor: navColor,
+                    iconData: Icons.fast_rewind,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClayContainer(
+                      color: navColor,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClayContainer(
+                          color: navColor,
+                          borderRadius: 50,
+                          child: Icon(
+                            Icons.play_arrow,
+                            size: 62,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ClayButtonWidget(
+                    baseColor: navColor,
+                    iconData: Icons.fast_forward,
+                  ),
+                  ClayButtonWidget(
+                    baseColor: navColor,
+                    iconData: Icons.skip_next,
+                  ),
+                ],
               ),
             ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ClayButtonWidget extends StatelessWidget {
+  const ClayButtonWidget({
+    Key key,
+    @required this.baseColor,
+    this.iconData,
+    this.size = 62,
+  }) : super(key: key);
+
+  final Color baseColor;
+  final IconData iconData;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClayContainer(
+        color: baseColor,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+            iconData,
+            size: size,
+            color: Color(0xFF7b7b7b),
           ),
         ),
       ),
